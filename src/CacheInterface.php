8807aa5bb016f66ded7b4b8baa1cee993f7a7d38
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-02-18 22:27:27 +0100

    Expanded CacheInterface documentation

--- src/CacheInterface.php
+++ src/CacheInterface.php
@@ -4,6 +4,11 @@ namespace React\Cache;
 
 use React\Promise\PromiseInterface;
 
+/**
+ * All methods only reject with an exception when an error occurs on the underlying storage
+ * layer, for example the connection with Redis dropped and cannot be recovered, or the
+ * directory on the filesystem used to store cache has the wrong permissions of is full.
+ */
 interface CacheInterface
 {
     /**
@@ -16,19 +21,22 @@ interface CacheInterface
     public function get($key);
 
     /**
-     * Store an item in the cache.
+     * Store an item in the cache, returns a promise which resolves to true on success or
+     * rejects with an exception on error.
      *
      * @param string $key
      * @param mixed $value
-     * @return PromiseInterface
+     * @return PromiseInterface<bool|Exception>
      */
     public function set($key, $value);
 
     /**
-     * Remove an item from the cache.
+     * Remove an item from the cache, returns a promise which resolves to true on success or
+     * rejects with an exception on error. When the $key isn't found in the cache it also
+     * resolves true.
      *
      * @param string $key
-     * @return PromiseInterface
+     * @return PromiseInterface<bool|Exception>
      */
     public function remove($key);
 }
